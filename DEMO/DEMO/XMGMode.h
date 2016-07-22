c87934292d5a426325cf8f838066729f876baa0e
Elon <mac@elons-blackapple.local>
2016-07-22 15:15:46 +0800

    创建了mode类

---
+++ DEMO/DEMO/XMGMode.h
@@ -0,0 +1,13 @@
+//
+//  XMGMode.h
+//  DEMO
+//
+//  Created by Elon on 16/7/22.
+//  Copyright © 2016年 XMG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XMGMode : NSObject
+
+@end
---
+++ DEMO/DEMO/XMGMode.m
@@ -0,0 +1,13 @@
+//
+//  XMGMode.m
+//  DEMO
+//
+//  Created by Elon on 16/7/22.
+//  Copyright © 2016年 XMG. All rights reserved.
+//
+
+#import "XMGMode.h"
+
+@implementation XMGMode
+
+@end
